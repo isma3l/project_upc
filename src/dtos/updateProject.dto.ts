93442b644f37c21e60b4a6976718ae5f76e965e4
isma3l <karlo.ismael@gmail.com>
2022-11-08 01:32:03 +0100

    actualizando swagger y se permite nombre de proyectos con espacios

--- src/dtos/updateProject.dto.ts
+++ src/dtos/updateProject.dto.ts
@@ -2,7 +2,7 @@ import { IsOptional, IsString, Matches } from 'class-validator';
 
 export class UpdateProjectDto {
   @IsString()
-  @Matches(/^[A-Za-z][A-Za-z0-9_-]{3,30}$/)
+  @Matches(/^[A-Za-z][A-Za-z0-9_-\s]{3,30}$/)
   public name: string;
 
   @IsOptional()
